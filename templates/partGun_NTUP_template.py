53278646fd3abcbfd6073b06eb07b1c00ae78527
Clemens Lange <clemens.lange@cern.ch>
2017-02-28 17:55:41 +0100

    adjust config for changes in ntupliser

--- templates/partGun_NTUP_template.py
+++ templates/partGun_NTUP_template.py
@@ -2,11 +2,17 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.Eras import eras
 
 process = cms.Process("Demo")
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('Configuration.Geometry.GeometryExtended2023D4Reco_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('RecoLocalCalo.HGCalRecProducers.HGCalLocalRecoSequence_cff')
 #process.load("RecoLocalCalo.HGCalRecProducers.hgcalLayerClusters_cfi")
+
+from FastSimulation.Event.ParticleFilter_cfi import *
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(DUMMYEVTSPERJOB) )
 
@@ -20,16 +26,32 @@ process.source = cms.Source("PoolSource",
 
 process.ana = cms.EDAnalyzer('HGCalAnalysis',
                              detector = cms.string("all"),
-                             rawRecHits = cms.bool(True)
+                             rawRecHits = cms.bool(True),
+                             readOfficialReco = cms.bool(False),
+                             readCaloParticles = cms.bool(False),
+                             layerClusterPtThreshold = cms.double(0.01),  # All LayerCluster belonging to a multicluster are saved; this Pt threshold applied to the others
+                             TestParticleFilter = ParticleFilterBlock.ParticleFilter
 )
+
+process.ana.TestParticleFilter.protonEMin = cms.double(100000)
+process.ana.TestParticleFilter.etaMax = cms.double(3.1)
+
 
 process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string("file:DUMMYFILENAME")
 
                                    )
 
-#process.hgcalLayerClusters.minClusters = cms.uint32(0)
-#process.hgcalLayerClusters.realSpaceCone = cms.bool(True)
+reRunClustering = False
 
-process.p = cms.Path(process.ana)
-#process.p = cms.Path(process.hgcalLayerClusters+process.ana)
+if reRunClustering:
+    process.hgcalLayerClusters.minClusters = cms.uint32(0)
+    process.hgcalLayerClusters.realSpaceCone = cms.bool(True)
+    process.hgcalLayerClusters.multiclusterRadius = cms.double(2.)  # in cm if realSpaceCone is true
+    process.hgcalLayerClusters.dependSensor = cms.bool(True)
+    process.hgcalLayerClusters.ecut = cms.double(3.)  # multiple of sigma noise if dependSensor is true
+    process.hgcalLayerClusters.kappa = cms.double(9.)  # multiple of sigma noise if dependSensor is true
+    #process.hgcalLayerClusters.deltac = cms.vdouble(2.,3.,5.) #specify delta c for each subdetector separately
+    process.p = cms.Path(process.hgcalLayerClusters+process.ana)
+else:
+    process.p = cms.Path(process.ana)
