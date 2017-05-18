54c6eec0d614c72b2893f1df6327227908e6f71f
Clemens Lange <clemens.lange@cern.ch>
2017-05-18 16:56:42 +0200

    update config templates to D13

--- templates/partGun_NTUP_template.py
+++ templates/partGun_NTUP_template.py
@@ -4,8 +4,8 @@ from Configuration.StandardSequences.Eras import eras
 process = cms.Process("Demo")
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
-process.load('Configuration.Geometry.GeometryExtended2023D4Reco_cff')
-process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D13Reco_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
@@ -21,11 +21,11 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
         DUMMYINPUTFILELIST
     ),
-    duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
-    inputCommands=cms.untracked.vstring(
-        'keep *',
-        'drop EcalEBTriggerPrimitiveDigisSorted_simEcalEBTriggerPrimitiveDigis__HLT'
-    )
+    duplicateCheckMode = cms.untracked.string("noDuplicateCheck")
+    # inputCommands=cms.untracked.vstring(
+    #     'keep *',
+    #     'drop EcalEBTriggerPrimitiveDigisSorted_simEcalEBTriggerPrimitiveDigis__HLT'
+    # )
 )
 
 process.ana = cms.EDAnalyzer('HGCalAnalysis',
@@ -33,7 +33,7 @@ process.ana = cms.EDAnalyzer('HGCalAnalysis',
                              rawRecHits = cms.bool(True),
                              readOfficialReco = cms.bool(DUMMYROR),
                              readCaloParticles = cms.bool(False),
-                             layerClusterPtThreshold = cms.double(0.01),  # All LayerCluster belonging to a multicluster are saved; this Pt threshold applied to the others
+                             layerClusterPtThreshold = cms.double(-1),  # All LayerCluster belonging to a multicluster are saved; this Pt threshold applied to the others
                              TestParticleFilter = ParticleFilterBlock.ParticleFilter
 )
 
@@ -46,15 +46,15 @@ process.TFileService = cms.Service("TFileService",
 
                                    )
 
-reRunClustering = False
+reRunClustering = True
 
 if reRunClustering:
-    process.hgcalLayerClusters.minClusters = cms.uint32(0)
-    process.hgcalLayerClusters.realSpaceCone = cms.bool(True)
-    process.hgcalLayerClusters.multiclusterRadius = cms.double(2.)  # in cm if realSpaceCone is true
-    process.hgcalLayerClusters.dependSensor = cms.bool(True)
-    process.hgcalLayerClusters.ecut = cms.double(3.)  # multiple of sigma noise if dependSensor is true
-    process.hgcalLayerClusters.kappa = cms.double(9.)  # multiple of sigma noise if dependSensor is true
+    # process.hgcalLayerClusters.minClusters = cms.uint32(0)
+    # process.hgcalLayerClusters.realSpaceCone = cms.bool(True)
+    # process.hgcalLayerClusters.multiclusterRadius = cms.double(2.)  # in cm if realSpaceCone is true
+    # process.hgcalLayerClusters.dependSensor = cms.bool(True)
+    # process.hgcalLayerClusters.ecut = cms.double(3.)  # multiple of sigma noise if dependSensor is true
+    # process.hgcalLayerClusters.kappa = cms.double(9.)  # multiple of sigma noise if dependSensor is true
     #process.hgcalLayerClusters.deltac = cms.vdouble(2.,3.,5.) #specify delta c for each subdetector separately
     process.p = cms.Path(process.hgcalLayerClusters+process.ana)
 else:
