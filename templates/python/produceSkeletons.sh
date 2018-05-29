11616ea77433773b71e9747793d55c61314a9bc8
Clemens Lange <clemens.lange@cern.ch>
2018-05-29 13:44:52 +0200

    clean up D23 template script, add more information

--- templates/python/produceSkeletons.sh
+++ templates/python/produceSkeletons.sh
@@ -2,17 +2,34 @@
 
 # This is the shell script that will generate all the skeletons using cmsDriver commands.
 # The commands included have been taken from runTheMatrix with the following command:
-
+#
 # runTheMatrix.py -w relval_upgrade -j 0 -l 22088
-
+#
+# For all commands remove --filein and --fileout options.
+# Add python_filename option
+#
+# The first command combines step1 and step2 (GSD):
+# - run up to DIGI...HLT:@fake2
+# The following changes are implemented on top:
+# --eventcontent FEVTDEBUG ➜ --eventcontent FEVTDEBUGHLT
+#
+# The second command is step3 removing overlap with step2 (RECO):
+# - also remove MINIAODSIM, PAT
+# - remove from VALIDATION@miniAODValidation
+# - remove from DQM:@miniAODDQM
+#
+# The third command is a copy of the second only re-running RECO (for NTUP):
+# - remove DQM
+# - add processName=NTUP option
+#
 # Those commands should be regularly checked and, in case of changes, propagated into this script!
 
 
 cmsDriver.py SinglePiPt25Eta1p7_2p7_cfi  --conditions auto:phase2_realistic \
-  -n 10 --era Phase2 --eventcontent FEVTDEBUG --nThreads 4 \
+  -n 10 --era Phase2 --eventcontent FEVTDEBUGHLT --nThreads 4 \
   -s GEN,SIM,DIGI:pdigi_valid,L1,L1TrackTrigger,DIGI2RAW,HLT:@fake2 \
   --datatier GEN-SIM --beamspot HLLHC \
-  --geometry Extended2023D23 --era Phase2 --eventcontent FEVTDEBUGHLT \
+  --geometry Extended2023D23 --era Phase2 \
   --no_exec --python_filename=GSD_fragment.py
 
 
@@ -22,12 +39,10 @@ cmsDriver.py step3  --conditions auto:phase2_realistic -n 10 \
   --datatier GEN-SIM-RECO,DQMIO --geometry Extended2023D23 \
   --no_exec --python_filename=RECO_fragment.py
 
+
 cmsDriver.py step3 --conditions auto:phase2_realistic -n 10 \
   --era Phase2 --eventcontent FEVTDEBUGHLT --runUnscheduled \
   -s RAW2DIGI,L1Reco,RECO,RECOSIM \
   --datatier GEN-SIM-RECO --geometry Extended2023D23 \
   --no_exec --python_filename=NTUP_fragment.py \
   --processName=NTUP
-
-
-
