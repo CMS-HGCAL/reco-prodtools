06c4ad47d05dcd0b3a75c6a5b050a62c888ca19e
b-fontana <bruno.afonso.alves@ist.utl.pt>
2019-06-12 16:27:53 +0200

    bash version of zshell script (#61)
    * added directories as input arguments

--- submitNextStep.sh
+++ submitNextStep.sh
@@ -1,31 +1,54 @@
-#!/bin/zsh
-
+#!/usr/bin/env bash
 #
 # Script to automatically submit jobs for next step (RECO or NTUP) for all
 # samples in SAMPLEDIR. Can adjust events per job with variables listed
-# below (EVTSRECO and EVTSNTUP).
+# below (EVTSRECO and EVTSNTUP). The folders where the GSD, RECO or NTUP folders
+#are in turn stored must be specified as follows:
 #
-
+# bash submitNextStep.sh folder1 folder2 ...
+#
 SAMPLEDIR='/eos/cms/store/cmst3/group/hgcal/CMG_studies/Production'
-EVTSRECO=60
+MYDIRS=( "$@" )
+EVTSRECO=1
 EVTSNTUP=120
 
-for i in `ls $SAMPLEDIR`; do
-    CONTENT=("${(@f)$(ls $SAMPLEDIR/$i)}")
-    echo "$i has $CONTENT"
-    if [[ ${CONTENT[(r)GSD]} == GSD ]]; then
-        if [[ ${CONTENT[(r)RECO]} == RECO ]]; then
-            if [[ ${CONTENT[(r)NTUP]} == NTUP ]]; then
-                echo "Nothing to be done"
+arrayContains() { 
+    # Checks whether a certain element belongs to the provided array
+    # Arguments:
+    # 1. Element to search in the array
+    # 2. Name of the array
+    local match="${1}"
+    shift
+    local array=("$@")
+    local flag=false;
+
+    for i in "${array[@]}"; do
+	if [ $i == "${match}" ] || [ $i == "${match}/" ]; then
+	    flag=true;
+	    break;
+	fi
+    done
+    echo $flag;
+}
+
+for item in `ls $SAMPLEDIR`; do
+    if [ $(arrayContains "${item}" "${MYDIRS[@]}") = true ]; then
+    	CONTENT=(`ls $SAMPLEDIR/$item`)
+    	if [ $(arrayContains "GSD" "${CONTENT[@]}") = true ]; then
+    	    if [ $(arrayContains "RECO" "${CONTENT[@]}") = true ]; then
+    		if [ $(arrayContains "NTUP" "${CONTENT[@]}") = true ]; then
+                    echo "${item}: Nothing to be done."
+    		else
+                    echo "${item}: submit NTUP."
+                    python SubmitHGCalPGun.py --datTier NTUP --evtsperjob $EVTSNTUP --queue workday --inDir $item --eosArea $SAMPLEDIR
+    		fi
             else
-                echo "submit NTUP for $i"
-                python SubmitHGCalPGun.py --datTier NTUP --evtsperjob $EVTSNTUP --queue workday --inDir $i
+    		echo "${item}: submit RECO."
+    		python SubmitHGCalPGun.py --datTier RECO --evtsperjob $EVTSRECO --queue longlunch --inDir $item --eosArea $SAMPLEDIR
             fi
-        else
-            echo "submit RECO for $i"
-            python SubmitHGCalPGun.py --datTier RECO --evtsperjob $EVTSRECO --queue workday --inDir $i
-        fi
-    else
-        echo "$i does not seem to be a directory containing event samples! Skipping."
+    	else
+            echo "$item does not seem to be a directory containing event samples (GSD)!"
+	    echo "Skipping."
+    	fi
     fi
 done
